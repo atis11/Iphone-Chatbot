11834df24f088f85a419054e2dd2c25314c9ca0f
atis11 <135610840+atis11@users.noreply.github.com>
2025-06-07 02:47:32 +0530

    conversational flow

--- app.py
+++ app.py
@@ -19,13 +19,33 @@ retriever = get_cached_retriever()
 groq_model = "llama-3.1-8b-instant"
 groq_token = st.secrets["groq_api_key"]
 
+#chat history list
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+#button to clear chat history
+if st.button("🗑️ Clear Chat History"):
+    st.session_state.chat_history = []
+    st.success("Chat history cleared!")
+# Display Chat history
+with st.expander(" Chat History", expanded=True):
+    if st.session_state.chat_history:
+        for i, (q, a) in enumerate(st.session_state.chat_history):
+            st.markdown(f"**User:** {q}")
+            st.markdown(f"**iBot:** {a}")
+            st.markdown("---")
+    else:
+        st.markdown("_No previous conversation yet._")
+
 query = st.text_area("Your question:")
 show_sources = st.checkbox("Show sources")
+
 
 if st.button("Ask"):
     with st.spinner("Generating answer..."):
         try:
-            answer, sources = ask_query(groq_model, query, groq_token, retriever)
+            
+            answer, sources = ask_query(groq_model, query, groq_token, retriever,st.session_state.chat_history)
+            st.session_state.chat_history.append((query, answer))
             st.markdown("### Answer")
             st.write(answer)
             
